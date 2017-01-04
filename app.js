d207b72b3b47237c40bda21c2b572082ab449a16
Jamie King <king0120@gmail.com>
2017-01-03 19:02:31 -0500

    able to save user now

--- app.js
+++ app.js
@@ -1,14 +1,44 @@
 var express = require('express')
 var app = express();
 var mongoose = require('mongoose');
-var User = require('./schema');
-
 mongoose.connect('mongodb://localhost/weathervain');
+require('./schema')(mongoose);
+const db = mongoose.connection;
+
+var User = mongoose.model('User');
+
+db.on('error', (err) => {
+  console.error(`connection error: ${err}`);
+  process.exit(1);
+})
+
+db.on('open', () => {
+  console.log('Connected to MongoDb')
+})
+
+
 
 app.get('/', function (req, res) {
-  User.find({}, (err, users) => {
-    res.json(users);
+  var bob = new User({
+    name: 'Booob',
+    username: 'ffd',
+    password: 'password'
+  });
+
+  bob.save((err) => {
+    if (err) {
+      console.log(err)
+      throw err
+    };
+
+    console.log('user saved successfully');
+    User.find({}, (err, users) => {
+      if (err) throw err;
+      res.json(users);
+    });
   })
+
+
 })
 
 app.listen(3000, function () {
--- schema.js
+++ schema.js
@@ -1,27 +1,12 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+module.exports = function (mongoose) {
+  var mongoose = require('mongoose');
+  var Schema = mongoose.Schema;
 
-var userSchema = new Schema({
-  name: String,
-  username: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  admin: Boolean,
-  created: Date
-});
-
-userSchema.pre('save', (next) => {
-  var currentDate = new Date();
-
-  this.created = currentDate;
-})
-
-var User = mongoose.model('User', userSchema);
-
-module.exports = User;
+  var userSchema = new Schema({
+    name: String,
+    username: String,
+    password: String
+  });
+  
+  mongoose.model('User', userSchema);
+}
